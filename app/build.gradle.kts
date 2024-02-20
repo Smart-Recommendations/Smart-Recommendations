501c40e061ea5ae7a07d9ee5c3a8f51afb698b93
entrylevelcs <mojo2556@gmail.com>
2024-02-20 14:31:52 -0700

    Fixed an issue that prevented the app to be built as an APK.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    constraints {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") {
+            because("kotlin-stdlib-jdk7 is now a part of kotlin-stdlib")
+        }
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0") {
+            because("kotlin-stdlib-jdk8 is now a part of kotlin-stdlib")
+        }
+    }
 }

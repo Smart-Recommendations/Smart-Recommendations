b3b30e14c7154ec04983199f972ac5d7fd88a2fa
tluke3 <tluke3@msudenver.edu>
2024-02-22 15:02:32 -0700

    Add Compose to project

---
+++ app/src/main/java/com/example/smartrecommendations/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.smartrecommendations.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val white = Color(0xFFFFFFFF)
+val darkGrey = Color(0xFF202020)
+
+private val DarkColors = darkColorScheme(
+        primary = Purple80,
+        secondary = PurpleGrey80,
+        tertiary = Pink80,
+)
+
+private val LightColors = lightColorScheme(
+        primary = Purple40,
+        secondary = PurpleGrey40,
+        tertiary = Pink40,
+)
